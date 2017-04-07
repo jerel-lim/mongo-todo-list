95d10741ebedb33ebc2c7c58bb262cb0234176c0
Jerel Lim <j.limster@gmail.com>
2017-04-07 08:05:12 +0800

    hw completed

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,20 +1,120 @@
 // TODO. import TODO Model ;-)
-// const Todo = require('../models/todo')
+const Todo = require('../models/todo')
+var mongoose = require('mongoose')
+var dbURI = 'mongodb://localhost/todo'
+mongoose.Promise = global.Promise
 
 function create (params) {
   // create a new TODO and console log the response
+  mongoose.connect(dbURI)
+  var newTodo = new Todo()
+
+  newTodo.name = params.name
+  newTodo.description = params.description
+  newTodo.completed = params.completed
+
+  if (!params.description || params.description === '') {
+    params.description = 'default description'
+  }
+  if (!params.completed || params.completed === '') {
+    params.completed = false
+  }
+  if (params.name.length >= 5) {
+    newTodo.save(function (err, data) {
+      if (err) console.error(err)
+      console.log(data)
+      mongoose.disconnect()
+    })
+  } else {
+    mongoose.disconnect()
+    // return false
+  }
 }
+
 function list () {
   // console log the list of all TODOs
+  mongoose.connect(dbURI)
+  Todo.find({}, function (err, data) {
+    if (err) {
+      console.error(err)
+      mongoose.disconnect()
+    } else {
+      console.log(data)
+      mongoose.disconnect()
+    }
+  })
 }
 function show (id) {
   // find the TODO with this id and console log it
+mongoose.connect(dbURI)
+  Todo.find().where('_id').equals(id).exec(function (err, data) {
+    if (err) {
+      console.error(err)
+      mongoose.disconnect()
+    } else {
+      console.log(data)
+      mongoose.disconnect()
+    }
+  })
 }
+
 function update (id, params) {
   // find the TODO with this id and update it's params. console log the result.
+  mongoose.connect(dbURI)
+  if (params.name.length >= 5) {
+    Todo.findByIdAndUpdate(id, {$set: params}, {new: true}, function (err, data) {
+      if (err) {
+        console.error(err)
+        mongoose.disconnect()
+        // return false
+      }
+      else{
+        console.log(data)
+        mongoose.disconnect()
+        // return true
+      }
+    })
+  }
+  else {
+    mongoose.disconnect()
+    // return false
+  }
+
+
+
+
 }
 function destroy (id) {
   // find the TODO with this id and destroy it. console log success/failure.
+  mongoose.connect(dbURI)
+  Todo.findByIdAndRemove(id, function (err, data) {
+      if (err) {
+        console.error(err)
+        mongoose.disconnect()
+        // return false
+      }
+      else{
+        console.log(data)
+        mongoose.disconnect()
+        // return true
+      }
+    })
+}
+
+function destroyAll () {
+  mongoose.connect(dbURI)
+  Todo.remove({}, function (err, data) {
+    if (err) {
+      console.error(err)
+      mongoose.disconnect()
+      // return false
+    }
+    else{
+      console.log(data)
+      mongoose.disconnect()
+      // return true
+    }
+  })
 }
 
 module.exports = {
@@ -22,5 +122,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
--- models/todo.js
+++ models/todo.js
@@ -1,1 +1,12 @@
 // Mongoose Schema and Models goes here
+var mongoose = require('mongoose')
+
+var todoSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  completed: Boolean
+})
+
+var Todo = mongoose.model('Todo', todoSchema)
+
+module.exports = Todo
